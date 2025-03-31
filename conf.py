6ffb6de3108991ed8d269b0a5c05c3cc3a302552
Blezix <Jakubmilosz1@gmail.com>
2025-03-31 08:34:40 +0200

    sdjiasj

---
+++ conf.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Nazwa Projektu'
+copyright = '2025, Twoje Imię'
+author = 'Twoje Imię'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+html_theme = 'sphinx_rtd_theme'
